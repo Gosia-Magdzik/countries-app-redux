977f3c173ac23d5968dcc338fc4b8e7aeace5e7a
Gosia Magdzik <melassa26@gmail.com>
2023-08-12 13:42:23 +0200

    new All Countries component added

--- src/App.js
+++ src/App.js
@@ -4,7 +4,8 @@ import { ThemeProvider } from "styled-components";
 import { darkTheme, lightTheme } from "./theme";
 import { selectIsDarkMode } from "./Components/NavigationBar/ModeSwither/ModeSlice";
 import { useSelector } from "react-redux";
-import { GlobalStyle } from "./GlobalStyles";
+import { GlobalStyle, Container } from "./GlobalStyles";
+import { AllCountries } from "./Components/AllCountries"
 
 function App () {
   const darkMode = useSelector(selectIsDarkMode);
@@ -13,6 +14,9 @@ function App () {
     <ThemeProvider theme={ darkMode ? darkTheme : lightTheme }>
       <GlobalStyle/>
       <Nav/>
+      <Container>
+        <AllCountries/>
+      </Container>
     </ThemeProvider>
   );
 };
