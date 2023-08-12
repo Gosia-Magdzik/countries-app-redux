2910b05066531c143a8b969f0dfae795a2fa25e5
Gosia Magdzik <melassa26@gmail.com>
2023-08-12 14:25:46 +0200

    all countries component

--- src/Components/AllCountries/index.js
+++ src/Components/AllCountries/index.js
@@ -1,6 +1,31 @@
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import {
+    fetchCountries,
+    selectCountries,
+    selectStatus
+} from "./countriesSlice";
+import { Loader } from "../Loading";
+
 
 export const AllCountries = () => {
+    const dispatch = useDispatch();
+    const countries = useSelector(selectCountries);
+    const status = useSelector(selectStatus);
+
+    useEffect(() => {
+        dispatch(fetchCountries());
+    }, []);
+
     return (
-        <p>All</p>
+        <>
+        {status === "loading" ? (
+            <Loader/>
+        ) : status === "succes" ? (
+            countries.map((country) => (
+                <p>...</p>
+            ))
+        ) : null}
+        </>
     );
 };
