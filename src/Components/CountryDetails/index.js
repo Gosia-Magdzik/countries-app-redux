c76f5201bb661637ca4c09eb210fd6de090afb74
Gosia Magdzik <melassa26@gmail.com>
2023-08-12 19:22:19 +0200

    details

--- src/Components/CountryDetails/index.js
+++ src/Components/CountryDetails/index.js
@@ -1,10 +1,15 @@
 import { useParams } from "react-router-dom";
 import { 
-        Button, 
-        ButtonLink, 
-        Wrapper, 
-        Img } 
-        from "./styled";
+    Button, 
+    ButtonLink, 
+    Wrapper, 
+    Img,
+    Data,
+    Details, 
+    Name, 
+    Col, 
+    Paragraph } 
+from "./styled";
 import { ReactComponent as LeftArrowLight } from './arrow_left_white.svg';
 import { Loader } from "../Loading";
 import { useData } from "./useData";
@@ -37,7 +42,23 @@ export const Country = () => {
             ) : (
                 <>
                 <Img src={data.flag}/>
-                <p>{data.commonName}</p>
+                <Data>
+                <Name>{data.commonName}</Name>
+              <Details>
+                  <Col>
+                    <Paragraph><b>Native Name:</b>&nbsp;{data.officialName}</Paragraph>
+                    <Paragraph><b>Population:</b>&nbsp;{new Intl.NumberFormat().format(data.population)}</Paragraph>
+                    <Paragraph><b>Region:</b>&nbsp;{data.region}</Paragraph>
+                    <Paragraph><b>Sub Region:</b>&nbsp;{data.subRegion}</Paragraph>
+                    <Paragraph><b>Capital:</b>&nbsp;{data.capital}</Paragraph>
+                  </Col>
+                  <Col>
+                    <Paragraph><b>Top Level Domain:</b>&nbsp;{data.tld}</Paragraph>
+                    <Paragraph><b>Currencies:</b>&nbsp;{Object.values(data.currencies).map(currency => currency.name).join(', ')}</Paragraph>
+                    <Paragraph><b>Languages:</b>&nbsp;{Object.values(data.languages).join(', ')}</Paragraph>
+                  </Col>
+              </Details>
+                </Data>
                 </>
             )}
         </Wrapper>
