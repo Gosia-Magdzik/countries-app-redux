a2b1710afc00d78c1079ecd19928c82d5d9d51d2
Gosia Magdzik <melassa26@gmail.com>
2023-08-12 15:46:03 +0200

    store updated

--- src/store.js
+++ src/store.js
@@ -1,10 +1,18 @@
 import createSagaMiddleware from "redux-saga";
 import darkModeReducer from "./Components/NavigationBar/ModeSwither/ModeSlice";
-
+import countriesReducer from "./Components/AllCountries/countriesSlice";
 import { configureStore } from "@reduxjs/toolkit";
+import { countriesSaga } from "./Components/AllCountries/countriesSaga";
+
+const sagaMiddleware = createSagaMiddleware();
 
 export const store = configureStore({
     reducer: {
         darkMode: darkModeReducer,
-    }
-})
+        countries: countriesReducer,
+    },
+    middleware: [sagaMiddleware],
+});
+
+
+sagaMiddleware.run(countriesSaga);
